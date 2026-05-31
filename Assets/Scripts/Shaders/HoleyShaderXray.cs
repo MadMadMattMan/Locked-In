c6728230e248cd529754a3ab4d9dddd4dbf9dfd0
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2026-05-31 20:46:50 +1200

    Simple xRay/ascii fog example

---
+++ Assets/Scripts/Shaders/HoleyShaderXray.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using UnityEngine;
+
+public class HoleyShaderXray : MonoBehaviour {
+    public float fogDensity = 1;
+    float fogLerp;
+
+    public Camera playerCamera;
+    public Transform xRayFocusObject;
+    public float falloff = 0.1f;
+
+    public float maxSize = 5;
+    float currentSize = 0;
+    float targetSize = 0;
+
+    public float changeTime = 0.1f;
+
+    Material shaderMaterial;
+    Renderer targetRenderer;
+
+    Vector2 cutoutPos = new Vector2();
+
+
+    private void Start() {
+        shaderMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void Update() {
+        bool do_xRay = false;
+        if (xRayFocusObject != null) {
+            targetRenderer = xRayFocusObject.GetComponent<MeshRenderer>();
+            do_xRay = targetRenderer.isVisible;
+            if (do_xRay) {
+                RaycastHit hitdata;
+                Physics.Raycast(transform.position, (xRayFocusObject.position - transform.position).normalized * 100f, out hitdata);
+                do_xRay = hitdata.collider.gameObject == xRayFocusObject.gameObject;
+                Debug.DrawRay(transform.position, (xRayFocusObject.position - transform.position).normalized * 100f, Color.aliceBlue);
+            }
+        }
+
+        if (do_xRay)
+        {
+            if (targetSize == 0f) {
+                setCutoutState(true);
+            }
+
+            cutoutPos = playerCamera.WorldToViewportPoint(xRayFocusObject.position);
+            cutoutPos.y /= (Screen.width / Screen.height);
+
+            float cutoutSize = Mathf.Sqrt(Vector3.Distance(transform.position, xRayFocusObject.position)) * (currentSize / 1000);
+            shaderMaterial.SetFloat("_cutoutSize", cutoutSize);
+
+            shaderMaterial.SetVector("_cutoutPos", cutoutPos);
+            shaderMaterial.SetFloat("_cutoutFalloff", falloff);
+            return;
+        }
+        else if (targetSize == maxSize)
+            setCutoutState(false);
+
+        shaderMaterial.SetFloat("_cutoutSize", 0f);
+    }
+
+    void setCutoutState(bool state) {
+        StopAllCoroutines();
+        StartCoroutine(changeCutoutState(state));
+    }
+
+    IEnumerator changeCutoutState(bool state) {
+        targetSize = state ? maxSize : 0f;
+        float start = state ? 0f : maxSize;
+        float timer = state ? (currentSize / maxSize) : ((maxSize / currentSize)-1)*-1;
+        timer *= changeTime;
+
+        while (timer < changeTime) {
+            timer += Time.deltaTime;
+            currentSize = Mathf.Lerp(start, targetSize, timer/changeTime);
+            yield return new WaitForEndOfFrame();
+        }
+        currentSize = state ? maxSize : 0f;
+    }
+}
