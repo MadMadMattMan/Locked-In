56920aadc691b403dbda3b4208832de08556034f
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2026-06-01 22:49:04 +1200

    timeline merger

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    PlayableDirector pd;
+    PlayableAsset pa;
+    Animator a;
+
+    private void Start()
+    {
+
+    }
+}
