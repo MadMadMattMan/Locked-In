4ebe155026b52077026dcbb82e1b9058cef28648
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2026-06-04 12:22:34 +1200

    AudioPlayer.cs update

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
     public bool playOnce = false;
+    bool played;
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "Character") {
+            if (playOnce && played)
+                return;
+            played = true;
             GetComponent<AudioSource>().Play();
-            if (playOnce)
-                gameObject.SetActive(false);
         }
     }
 }
