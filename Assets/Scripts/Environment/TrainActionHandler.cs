62434bcb89ca29a2f33d323ee34a2e3cf604f2b0
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2026-06-05 11:25:03 +1200

    Train basics + checkpoints

--- Assets/Scripts/Environment/TrainActionHandler.cs
+++ Assets/Scripts/Environment/TrainActionHandler.cs
@@ -1,5 +1,16 @@
 using UnityEngine;
 
 public class TrainActionHandler : MonoBehaviour {
-    
+
+    public int selectedPath = 0;
+    bool pathSelectionAvailable = false;
+
+
+    public bool SelectPath(int i) {
+        if (!pathSelectionAvailable)
+            return false;
+
+        selectedPath = i;
+        return true;
+    }
 }
